20b4c65b50ad195d46a1c9da0af42a0c8caf9c3e
ouyang <ouyangchenling@cangoonline>
2018-08-24 17:45:49 +0800

    1.生成service base与 api base 工程

--- main-service/src/main/java/com/oycl/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/com/oycl/mainservice/MainServiceApplication.java
@@ -3,9 +3,11 @@ package com.oycl.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.oycl"})
 public class MainServiceApplication {
 
     public static void main(String[] args) {
