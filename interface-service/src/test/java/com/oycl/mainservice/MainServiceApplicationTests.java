584cfd25fa3a6e6614e7bae84ed6568930a24982
ouyang <ouyangchenling@cangoonline>
2018-08-27 18:10:48 +0800

    1.更新共通环境

---
+++ interface-service/src/test/java/com/oycl/mainservice/MainServiceApplicationTests.java
@@ -0,0 +1,26 @@
+package com.oycl.mainservice;
+
+import com.oycl.mainservice.service.impl.RetryServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainServiceApplicationTests {
+
+    @Autowired
+    private RetryServiceImpl retryService;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test1() throws Exception {
+        retryService.excute();
+    }
+
+}
