070aa1538823262058f1fc576738fe33c216fab1
欧阳晨灵 <ouyangchenling@cangoonline.com>
2019-02-21 13:23:51 +0800

    update

--- main-service/src/main/java/com/oycl/service/impl/RetryServiceImpl.java
+++ main-service/src/main/java/com/oycl/service/impl/RetryServiceImpl.java
@@ -5,21 +5,20 @@ import org.springframework.remoting.RemoteAccessException;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
-import util.retry.InterfaceRetryService;
+
 
 import java.util.Random;
 
 @Service
-public class RetryServiceImpl implements InterfaceRetryService {
+public class RetryServiceImpl {
 
     @Retryable(value= RemoteAccessException.class ,maxAttempts=2)
-    @Override
+
     public void excute() {
         System.out.println("调用excute方法");
         throw new NormalExciton("E003","业务异常");
     }
 
-    @Override
     @Recover
     public void recover(NormalExciton e) {
 
