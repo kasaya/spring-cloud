114892eaac5fff6a269a514dba5da2b94d0ac985
欧阳晨灵 <ouyangchenling@cangoonline.com>
2019-03-11 09:21:05 +0800

    追加 gateway

---
+++ spring-gateway-service/src/test/java/com/oycl/SpringGatewayServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oycl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringGatewayServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
