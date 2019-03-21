09c321db025923bee1e300a063fe251ed53d3064
欧阳晨灵 <ouyangchenling@cangoonline.com>
2019-03-21 19:55:41 +0800

    添加data service 实现

--- data-service/src/main/java/com/oycl/DataServiceApplication.java
+++ data-service/src/main/java/com/oycl/DataServiceApplication.java
@@ -1,11 +1,15 @@
 package com.oycl;
 
+import com.oycl.compment.db.annotation.EnabelBaseMapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.oycl"})
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnabelBaseMapper
+@MapperScan(basePackages = "com.oycl.dao")
 public class DataServiceApplication {
 
 	public static void main(String[] args) {
