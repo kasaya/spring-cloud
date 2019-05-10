e3a9f01bb6d54abcdb0882ec5651e5454b493a91
欧阳晨灵 <ouyangchenling@cangoonline.com>
2019-05-10 17:42:07 +0800

    追加流程中心

---
+++ activiti-service/src/main/java/com/oycl/ActivitiApplication.java
@@ -0,0 +1,15 @@
+package com.oycl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.oycl"})
+public class ActivitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiApplication.class, args);
+	}
+
+}
