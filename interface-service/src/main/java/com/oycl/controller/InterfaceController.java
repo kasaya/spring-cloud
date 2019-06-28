2f87988de852e896142f6302be74f9042e615e63
欧阳晨灵 <ouyangchenling@cangoonline.com>
2019-06-28 17:58:10 +0800

    更新 eureka配置 与 config 配置

--- interface-service/src/main/java/com/oycl/controller/InterfaceController.java
+++ interface-service/src/main/java/com/oycl/controller/InterfaceController.java
@@ -1,17 +1,11 @@
 package com.oycl.controller;
 
-import com.oycl.api.MainApi;
 import com.oycl.compment.log.annotation.CustomLog;
-import com.oycl.definition.SystemProperties;
 import com.oycl.interfaceserver.input.GetMcodeInput;
 import com.oycl.interfaceserver.output.GetMcodeOutPut;
-import com.oycl.mainserver.input.LoginInput;
-import com.oycl.mainserver.output.LoginOutput;
 import com.oycl.service.LogicService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,12 +19,6 @@ public class InterfaceController  {
     @Autowired
     LogicService logicService;
 
-    @Autowired
-    MainApi mainApi;
-
-    @Autowired
-    SystemProperties systemProperties;
-
     @RolesAllowed("R01")
     //@PreAuthorize("hasRole('R01')")
     @PostMapping(value = "/getmcode")
@@ -38,16 +26,16 @@ public class InterfaceController  {
         return logicService.getMcode(input);
     }
 
-    @GetMapping(value = "/cross")
+    /*@GetMapping(value = "/cross")
     public String cross(){
         LoginInput input = new LoginInput();
         LoginOutput output =  mainApi.login(input);
          return output.getResultMessage();
-    }
+    }*/
 
     @PostMapping(value = "/test")
     public String test(){
-        return String.valueOf(systemProperties.getWorkerId());
+        return "success";
     }
 
     @PostMapping(value = "/setDay", produces = MediaType.APPLICATION_JSON_VALUE)
